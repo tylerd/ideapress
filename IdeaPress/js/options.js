2ba25abfe07a8d510c3287960e34d2f3cf8a20ab
Tyler D <tylergd@gmail.com>
2013-03-21 14:33:23 -0500

    Updating Options for my blog

--- IdeaPress/js/options.js
+++ IdeaPress/js/options.js
@@ -14,21 +14,17 @@ Description: Configures the application and the modules.  Please go to https://g
         function() {
             ideaPress.options = {
                 appTitleImage: null,                      // App title image (approx. 600px x 80px)
-                appTitle: "IdeaPress",                    // App title text
+                appTitle: "Tyler's Azure Developer Blog", // App title text
                 cacheTime: 3600000,                       // Global cache time to try fetch   
-                mainUrl: "http://ideanotion.net",         // Main promoting site
-                privacyUrl: "http://ideanotion.net",      // Privacy URL
+                mainUrl: "http://blog.tylerdoerksen.com", // Main promoting site
+                privacyUrl: "http://miguelapps.azurewebsites.net",      // Privacy URL
                 modules: [
-                    { name: wordpressModule, options: { apiUrl: 'http://ideanotion.net/', title: "Pages", typeId: wordpressModule.PAGES, pageIds: [2, 546, 565] } },
-                    { name: wordpressModule, options: { apiUrl: 'http://ideanotion.net/', title: "Recent News", typeId: wordpressModule.MOSTRECENT, hubItemsCount : 7 } },
-                    { name: wordpressModule, options: { apiUrl: 'http://ideanotion.net/', title: "Bookmark", typeId: wordpressModule.BOOKMARKS } },
-                    { name: wordpresscomModule, options: { siteDomain: 'wordpressmetro.wordpress.com', title: "Pages", typeId: wordpresscomModule.PAGES, pageIds: [2, 9], clientId: '2131', clientSecret: 'b8OEIPyqH113smvoCpgrShM3wakwYALgPOoFUn3X8PA9Y3l2hslQCCKev51VvHsR' } },
-                    { name: wordpresscomModule, options: { siteDomain: 'wordpressmetro.wordpress.com', title: "Recent News", typeId: wordpresscomModule.MOSTRECENT, clientId: '2131', clientSecret: 'b8OEIPyqH113smvoCpgrShM3wakwYALgPOoFUn3X8PA9Y3l2hslQCCKev51VvHsR' } },
-                    { name: wordpresscomModule, options: { siteDomain: 'wordpressmetro.wordpress.com', title: "Tech", typeId: wordpresscomModule.CATEGORY, categoryId: "tech", clientId: '2131', clientSecret: 'b8OEIPyqH113smvoCpgrShM3wakwYALgPOoFUn3X8PA9Y3l2hslQCCKev51VvHsR' } },
-                    { name: wordpresscomModule, options: { siteDomain: 'wordpressmetro.wordpress.com', title: "Bookmark", typeId: wordpresscomModule.BOOKMARKS, clientId: '2131', clientSecret: 'b8OEIPyqH113smvoCpgrShM3wakwYALgPOoFUn3X8PA9Y3l2hslQCCKev51VvHsR' } }
+                    { name: wordpressModule, options: { apiUrl: 'http://blog.tylerdoerksen.com', title: "Pages", typeId: wordpressModule.PAGES, pageIds: [2, 891] } },
+                    { name: wordpressModule, options: { apiUrl: 'http://blog.tylerdoerksen.com', title: "Recent News", typeId: wordpressModule.MOSTRECENT, hubItemsCount: 7 } },
+                    { name: wordpressModule, options: { apiUrl: 'http://blog.tylerdoerksen.com', title: "Bookmark", typeId: wordpressModule.BOOKMARKS } },
                 ],
-                searchModule: { name: wordpressModule, options: { apiUrl: 'http://ideanotion.net/', title: "Search" } },
-                liveTileModule: { name: wordpressModule, options: { apiUrl: 'http://ideanotion.net/', title: "Live Tile" } },
+                searchModule: { name: wordpressModule, options: { apiUrl: 'http://blog.tylerdoerksen.com', title: "Search" } },
+                liveTileModule: { name: wordpressModule, options: { apiUrl: 'http://blog.tylerdoerksen.com', title: "Live Tile" } },
             };
         });
 })();
